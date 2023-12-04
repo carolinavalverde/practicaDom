57b4e44707981bb970e3f29328e1bc9503186da9
Caro Valverde <carolina.valverde255@gmail.com>
2023-12-04 15:52:24 -0300

    agregamos evento submit

--- js/app.js
+++ js/app.js
@@ -1,69 +1,58 @@
 const cambiarTitulo = () => {
-  const titulo = document.querySelector(`h1`);
-  // const titulo = document.querySelector(`#algunId`);
-  // const titulo = document.querySelector(`.nuevoTitulo`);
-  // const muchosParrafos = document.querySelectorAll(`p`);
-  // const muchosParrafos = document.getElementById(`nuevoTitulo`);
-  // const muchosParrafos = document.getElementsByClassName(`nuevoTitulo`);
-  // const muchosParrafos = document.getElementsByTagName(`p`);
+  const titulo = document.querySelector("h1");
+  // const titulo = document.querySelector('#'); selector de id
+  // const titulo = document.querySelector('.'); selector de clase
+  // const parrafos = document.querySelectorAll('p');
+
+  // const parrafos = document.getElementById('nuevoTitulo')
+  // const parrafos = document.getElementsByClassName('nuevoTitulo')
+  // const parrafos = document.getElementsByTagName('p')
   console.log(titulo);
-  // titulo.innerText = `Nuevo titulo de JS`;
-  titulo.innerHTML = `Nuevo titulo de <b>JS</b>`;
-  titulo.className = `display-3 text-primary`;
+  // titulo.innerText = 'Nuevo título desde JS'; no se pueden agregar etiquetas html
+  titulo.innerHTML = "Nuevo título desde <b>JS</b>";
+  titulo.className = "display-3 text-primary";
 };
 
 const verMas = () => {
-  if (btnVerMas[3].innerHTML === `Ver mas`) {
-    console.log(`Desde la funcion ver mas`);
-    //crear parrafo nuevo
-    const parrafoNuevo = document.createElement(`p`);
-    parrafoNuevo.innerText = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas ratione, amet
-  dolores maiores cupiditate optio beatae repudiandae eos numquam. Vel earum,
-  expedita sit magnam veniam aperiam deserunt doloribus aliquam quam maiores
-  delectus ipsa ex cumque placeat harum reiciendis? Atque vitae saepe voluptatibus
-  iusto, illum odio dignissimos, praesentium tempora quod optio nisi. Veritatis
-  ipsa voluptatum perspiciatis totam repudiandae a explicabo, tenetur soluta,
-  optio sit ad quae expedita aliquam ipsum dolor impedit reprehenderit labore at.
-  Reiciendis enim aliquam reprehenderit voluptatibus autem! Consequuntur unde
-  cumque sit corporis ullam provident, laudantium est dolorem neque perferendis.
-  In laboriosam, fuga qui delectus sed ea sint id asperiores fugiat dicta. Porro
-  quos voluptatum libero aut, consectetur excepturi nisi voluptatibus
-  reprehenderit deserunt similique, non accusamus quasi voluptates distinctio.
-  Autem incidunt cupiditate unde maxime, officia assumenda! Neque aperiam totam
-  molestias nobis perferendis ullam maiores laudantium eos a, ducimus
-  exercitationem. Eius ea quas accusamus optio voluptas nihil, debitis magnam
-  similique praesentium illo minima quasi, expedita harum, ipsum officiis est
-  rerum fugiat voluptatem tempore iusto dolores! Doloremque, quo illo itaque
-  necessitatibus pariatur illum modi, debitis placeat fugit corrupti, veniam minus
-  error sapiente aliquid eveniet fugiat ullam sint adipisci temporibus odio. Est
-  error delectus similique optio pariatur debitis nulla minus, dolore assumenda
-  quasi aspernatur rem, facere ad iusto nam voluptatem atque, sunt natus! Dolorum
-  magni, error hic quisquam, autem quis sit, ab vero quidem dignissimos fugiat
-  laboriosam tempore? Eligendi, ex magni vero itaque excepturi fugit dolorum atque
-  adipisci voluptate facilis officia, at nulla modi dolor harum sit ratione amet
-  eius quas aspernatur?`;
-    parrafoNuevo.className = `lead`;
+  // traigo mi elemento padre
+  const seccion = document.querySelector("#contenedorPadre");
+  // condicion para q se oculte el texto
+  if (btnVerMas[3].innerHTML === "Ver mas") {
+    console.log("desde la funcion ver mas");
+    // crear un parrafo nuevo
+    const parrafoNuevo = document.createElement("p");
+    parrafoNuevo.innerText =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti consectetur libero commodi, magni eligendi laudantium vel optio repudiandae magnam tempora nulla asperiores delectus fugit ex facere quod culpa possimus voluptatem sed provident voluptates eos autem. Quis, repudiandae eligendi perspiciatis maiores neque, earum sit accusantium incidunt at dolorum quae voluptates nisi nulla nemo, culpa vitae. Reprehenderit, nam. Explicabo vero autem odit doloremque molestiae id error ducimus, facilis nobis, voluptates quidem pariatur sint! Beatae quisquam ipsum esse cumque, facilis modi quo omnis vero harum, earum nam atque architecto eligendi blanditiis fuga, culpa odit maxime minima. Ex eum quia illo doloremque assumenda nam quas? Deleniti voluptatibus quos velit magni ducimus dolor quam enim. Accusantium, in rerum. Libero et officia laboriosam non quae nam placeat, sit ipsum expedita dicta cum, debitis corporis quaerat! Et tempore rerum similique aspernatur placeat quaerat dolorem, culpa maiores necessitatibus! Enim modi repellendus, numquam dolorem distinctio officia nisi omnis nobis veniam natus, perspiciatis earum. Deserunt, necessitatibus eaque! Fugit nisi incidunt aliquam voluptate dolorum, fuga id, eveniet enim reiciendis voluptatum ea officiis earum ut deleniti quibusdam odio consequuntur. Dolorum temporibus vitae pariatur reprehenderit, atque velit nobis quisquam maiores aperiam facilis quos. Praesentium eos totam amet accusamus reiciendis, harum animi sed vel nostrum? Similique, quia. Consequatur nisi nulla aspernatur ad minus, nihil, voluptatem obcaecati quod sapiente quos repellat quidem, voluptatibus ipsum laboriosam suscipit cum dignissimos ducimus hic voluptates consectetur asperiores expedita. Veritatis neque aliquid vero tenetur cupiditate natus, expedita saepe quia voluptatem corporis odio nostrum autem alias ab ducimus deserunt illo explicabo!";
+    parrafoNuevo.className = "lead";
 
-    //traigo mi nodo padre o elemnto padre
-    const seccion = document.querySelector(`#contenedorPadre`);
-    //   console.log(seccion);
-    //agregar un nodo hijo
-    //seccion.appendChild(parrafoNuevo); //se agrega el parrafo pero como ultimo hijo, al final
-    //seccion.prepend(parrafoNuevo); //se agrega al principio, como primer hijo
+    // agregar un nodo hijo
+    // seccion.appendChild(parrafoNuevo); // agrega nodo hijo al final
+    // seccion.prepend(parrafoNuevo) // agrego nodo hijo al principio
     seccion.insertBefore(parrafoNuevo, btnVerMas[3]);
 
-    btnVerMas[3].innerHTML = `Ocultar`;
-    btnVerMas[3].className = `btn btn-danger`;
+    // modificar texto del boton
+    btnVerMas[3].innerHTML = "Ocultar";
+    btnVerMas[3].className = "btn btn-danger my-3";
   } else {
-    // console.log(`aqui quiero eliminar el parrafo`);
+    console.log(seccion.children[3]);
     seccion.removeChild(seccion.children[3]);
-    btnVerMas[3].innerHTML = `Ver mas`;
-    btnVerMas[3].className = `btn btn-primary`;
+    btnVerMas[3].innerHTML = "Ver mas";
+    btnVerMas[3].className = "btn btn-primary";
   }
+};
 
-  const btnVerMas = document.getElementsByTagName(`button`);
-  console.log(btnVerMas[3]);
+const obtenerTexto = (e) => {
+  e.preventDefault();
+  const inputBusqueda = document.querySelector("#inputBusqueda");
+  console.log(inputBusqueda.value);
+};
 
-  // btnVerMas[3].addEventListener(`click`, ()=>{ verMas(`dsa`, `asd`, 23)});
-  btnVerMas[3].addEventListener(`click`, verMas);
-}
+const btnVerMas = document.getElementsByTagName("button");
+console.log(btnVerMas[3]);
+const formularioBusqueda = document.querySelector("form");
+
+// sintaxis cuando la funcion tiene parametros (se crea una funcion anonima)
+// btnVerMas[3].addEventListener('click', ()=>(verMas('das', 'asd', 33)));
+
+btnVerMas[3].addEventListener("click", verMas);
+formularioBusqueda.addEventListener("submit", obtenerTexto);
